a0293f8fe1f177e38c617727d6b192014ee908bf
Seunghoya <viva.primavera121@gmail.com>
2022-08-10 16:50:34 +0900

    response body controller 추가

---
+++ src/main/java/springproject/springmvc/basic/response/ResponseBodyController.java
@@ -0,0 +1,18 @@
+package springproject.springmvc.basic.response;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class ResponseBodyController {
+
+    @GetMapping("/response-body-string-v1")
+    public void responseBodyV1(HttpServletResponse res) throws IOException {
+        res.getWriter().write("ok");
+    }
+}
